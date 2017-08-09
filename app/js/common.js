e4eaba81174e42dc4b9dee782b09449d6168d6ac
Evgenii Pisnyi <pisnyi@example.ru>
2017-08-09 17:48:02 +0300

    start project

---
+++ app/js/common.js
@@ -0,0 +1,16 @@
+// Set options
+    var options = {
+        offset: '#showHere',
+        offsetSide: 'top',
+        classes: {
+        clone:   'banner--clone',
+        stick:   'banner--stick',
+        unstick: 'banner--unstick'
+    }
+};
+
+// Initialise with options
+var banner = new Headhesive('.banner', options);
+
+// Headhesive destroy
+// banner.destroy();
