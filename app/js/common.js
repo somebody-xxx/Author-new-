f15ec952ee420283d1e18a07e540a3b638fe3132
Evgenii Pisnyi <pisnyi@example.ru>
2017-08-13 18:11:34 +0300

    was added anchor script and added #content style

--- app/js/common.js
+++ app/js/common.js
@@ -16,3 +16,14 @@ var banner = new Headhesive('.banner', options);
 // Headhesive destroy
 // banner.destroy();
 /*------------------------menu-------------------------*/
+/*------------------------аnchor-------------------------*/
+
+ $(document).ready(function(){
+    $(".nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+/*------------------------аnchor-------------------------*/
