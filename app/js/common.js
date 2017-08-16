851a462146f974d52933ac9a06d63d7f1d9389b6
Evgenii Pisnyi <pisnyi@example.ru>
2017-08-16 14:53:25 +0300

    was added social_icons and changed style .ghost-button

--- app/js/common.js
+++ app/js/common.js
@@ -27,3 +27,13 @@ var banner = new Headhesive('.banner', options);
     });
 });
 /*------------------------аnchor-------------------------*/
+
+ $(document).ready(function(){
+    $(".nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+/*------------------------аnchor-------------------------*/
