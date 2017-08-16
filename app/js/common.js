7d4e800a90852e2f3dad0c6b6547b4dd38384872
Evgenii Pisnyi <pisnyi@example.ru>
2017-08-16 15:56:40 +0300

    was changed header, deleed style and js files, added 'position:fixed'

--- app/js/common.js
+++ app/js/common.js
@@ -1,31 +1,3 @@
-/*------------------------menu-------------------------*/
-// Set options
-    var options = {
-        offset: '#showHere',
-        offsetSide: 'top',
-        classes: {
-        clone:   'banner--clone',
-        stick:   'banner--stick',
-        unstick: 'banner--unstick'
-    }
-};
-
-// Initialise with options
-var banner = new Headhesive('.banner', options);
-
-// Headhesive destroy
-// banner.destroy();
-/*------------------------menu-------------------------*/
-/*------------------------аnchor-------------------------*/
-
- $(document).ready(function(){
-    $(".nav").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
-    });
-});
 /*------------------------аnchor-------------------------*/
 
  $(document).ready(function(){
