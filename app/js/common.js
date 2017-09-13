8089812b6f25c91a915a8942aca98cfe7531d225
Evgenii Pisnyi <pisnyi@example.ru>
2017-09-13 14:41:15 +0300

    was added function 'validate'

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,30 @@
+/*------------------------form--------------------------*/
+function validate(){
+   //Считаем значения из полей name и email в переменные x и y
+   var name=document.forms["form"]["name"].value;
+   var email=document.forms["form"]["email"].value;
+   //Если поле name пустое выведем сообщение и предотвратим отправку формы
+   if (name.length==0 || email.length==0){
+      document.getElementById("namef").innerHTML="*данное поле обязательно для заполнения";
+      document.getElementById("emailf").innerHTML="*данное поле обязательно для заполнения";
+      return false;
+   }
+}
+/*function validate_email(){
+    var adr_pattern=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/;
+    var email=document.forms["form"]["email"].value;
+
+    var prov=adr_pattern.test(email);
+
+    if(email.length >= 1){
+        //Если поле не содержит эти символы знач email введен не верно
+        if (prov == true){
+         document.getElementById("emailf").innerHTML="*email введен не верно";
+         return false;
+            }
+        }
+}*/
+/*------------------------form--------------------------*/
 /*------------------------аnchor-------------------------*/
 
  $(document).ready(function(){
@@ -9,6 +36,7 @@
     });
 });
 /*------------------------аnchor-------------------------*/
+/*------------------------menu--------------------------*/
 jQuery(document).ready(function($){
  
     /* Подготавливаем иконку меню */
@@ -21,3 +49,4 @@ jQuery(document).ready(function($){
     });
 
 });
+/*------------------------menu--------------------------*/
